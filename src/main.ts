45d30cef3f8f62d967cf1c7110380521bfd7777b
duyetvn <duyetvn@viettel.com.vn>
2023-07-21 16:59:01 +0700

    update

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,10 @@
 import ENVIRONMENTS from 'constants/environment';
 import app from './app';
-import AppDataSource from 'configs/orm.config';
+import AppDataSource from 'configs/typeorm.config';
 
 AppDataSource.initialize()
 	.then(() => {
 		console.log('Data Source has been initialized!');
-		// AppDataSource.runMigrations();
 	})
 	.catch((err) => {
 		console.error('Error during Data Source initialization', err);
